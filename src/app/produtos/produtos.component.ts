d49ca210bcb4edc4e23d1a1b261b6cdd3745d0da
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2023-10-27 00:05:01 -0300

    Criação do Component barra-pesquisa

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,21 +1,33 @@
 import { Component } from '@angular/core';
 import { IProduto, produtos } from '../produtos';
 import { ProdutosService } from '../produtos.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
-  styleUrls: ['./produtos.component.css']
+  styleUrls: ['./produtos.component.css'],
 })
 export class ProdutosComponent {
-
   produtos: IProduto[] | undefined;
 
   constructor(
-    private produtosService: ProdutosService
-  ) { }
+    private produtosService: ProdutosService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.produtos = this.produtosService.getAll();
+    const produtos = this.produtosService.getAll();
+    this.route.queryParamMap.subscribe((params) => {
+      const descricao = params.get('descricao')?.toLowerCase();
+
+      if (descricao) {
+        this.produtos = produtos.filter((produto) => produto.descricao.toLowerCase().includes(descricao));
+          return;
+      }
+
+      this.produtos = produtos;
+    });
   }
 }
+
